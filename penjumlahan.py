9fd1e0ecea21cb1697aca7158a46ff724f05ec76
yokilabs <oki.machine@gmail.com>
2024-09-21 11:30:29 +0700

    First commit

---
+++ penjumlahan.py
@@ -0,0 +1,8 @@
+def penjumlahan():
+    a = int(input("Masukkan angka pertama: "))
+    b = int(input("Masukkan angka kedua: "))
+    return a+b
+
+"""
+Jika sudah bikin code maka silahkan ctrl + s
+"""
